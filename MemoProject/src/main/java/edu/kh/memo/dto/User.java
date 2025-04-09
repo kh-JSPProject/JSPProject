06f9df68e9b56ec242d5f9f155ff4a0fe69e7ca9
leehoyeun <dlghdus100@gmail.com>
2025-04-09 17:56:56 +0900

    detail

--- MemoProject/src/main/java/edu/kh/memo/dto/User.java
+++ MemoProject/src/main/java/edu/kh/memo/dto/User.java
@@ -1,5 +1,18 @@
 package edu.kh.memo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
 
+	private int userNo;			//유저번호
+	private String userId;		//유저아이디
+	private String userPw;		//유저비밀번호
+	private String userName;	//유저이름
 }
