5586b8bb258a854a90cfab5a9c4b28cc86e967cc
ashtiger1999 <aa26164095@gmail.com>
2025-04-10 21:40:19 +0900

    last

--- create-account.sql
+++ create-account.sql
@@ -5,3 +5,14 @@ CREATE USER memo IDENTIFIED BY memo1234;
 GRANT RESOURCE, CONNECT TO memo;
 
 ALTER USER memo DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+
+	select user_no, user_pw, user_name
+	from tb_users;
+	
+
+SELECT memo_no, user_no, title, content, 
+	to_char(reg_date,'YYYY-MM-DD HH:MI') reg_date, to_char(update_date,'YYYY-MM-DD HH:MI')
+FROM tb_memos
+WHERE user_no = (SELECT user_no
+								 FROM TB_USERS
+								 WHERE user_id = 'hong01');
